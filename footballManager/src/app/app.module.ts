6b35ee366d41729463ed1fb3caa8c7e0b4c7baca
David <Davidgarrosa_7@hotmail.com>
2021-05-27 17:53:53 +0200

    creadas rutas de Angular

--- footballManager/src/app/app.module.ts
+++ footballManager/src/app/app.module.ts
@@ -3,25 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { LigaComponent } from './liga/liga.component';
-import { LigueComponent } from './ligue/ligue.component';
-import { TeamComponent } from './team/team.component';
-import { MarketComponent } from './market/market.component';
-import { TransferMarketComponent } from './transfer-market/transfer-market.component';
-import { SoccerDayComponent } from './soccer-day/soccer-day.component';
-import { PlayersComponent } from './players/players.component';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { LigueComponent } from './components/ligue/ligue.component';
+import { TeamComponent } from './components/team/team.component';
+import { TransferMarketComponent } from './Components/transfer-market/transfer-market.component'
+import { SoccerDayComponent } from './components/soccer-day/soccer-day.component';
+import { PlayersComponent } from './components/players/players.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    LigaComponent,
     LigueComponent,
     TeamComponent,
-    MarketComponent,
     TransferMarketComponent,
     SoccerDayComponent,
     PlayersComponent
